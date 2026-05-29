de015a368d2b26368bbd9d7b4359ec62ac6a440c
Classbit India <it.classbit@gmail.com>
2026-05-29 22:12:59 +0530

    Vercel frontend now working

--- Classbit-HRMS/client/src/utils/api.js
+++ Classbit-HRMS/client/src/utils/api.js
@@ -1,9 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-    baseURL: import.meta.env.VITE_API_BASE_URL 
-        ? `${import.meta.env.VITE_API_BASE_URL}/api` 
-        : '/api',
+    baseURL: '/api',
     headers: {
         'Content-Type': 'application/json',
     },
